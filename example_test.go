fa7d78437c6371ce2b5def800cf8d40bc8c0898f
Erik Hollensbe <github@hollensbe.org>
2021-01-26 10:59:03 -0800

    Fix tests
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- example_test.go
+++ example_test.go
@@ -7,11 +7,12 @@ package colorwriter_test
 import (
 	"fmt"
 	"os"
-	"text/tabwriter"
+
+	"github.com/erikh/colorwriter"
 )
 
 func ExampleWriter_Init() {
-	w := new(tabwriter.Writer)
+	w := new(colorwriter.Writer)
 
 	// Format in tab-separated columns with a tab stop of 8.
 	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
@@ -23,7 +24,7 @@ func ExampleWriter_Init() {
 	// Format right-aligned in space-separated columns of minimal width 5
 	// and at least one blank of padding (so wider column entries do not
 	// touch each other).
-	w.Init(os.Stdout, 5, 0, 1, ' ', tabwriter.AlignRight)
+	w.Init(os.Stdout, 5, 0, 1, ' ', colorwriter.AlignRight)
 	fmt.Fprintln(w, "a\tb\tc\td\t.")
 	fmt.Fprintln(w, "123\t12345\t1234567\t123456789\t.")
 	fmt.Fprintln(w)
@@ -40,7 +41,7 @@ func ExampleWriter_Init() {
 func Example_elastic() {
 	// Observe how the b's and the d's, despite appearing in the
 	// second cell of each line, belong to different columns.
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, '.', tabwriter.AlignRight|tabwriter.Debug)
+	w := colorwriter.NewWriter(os.Stdout, 0, 0, 1, '.', colorwriter.AlignRight|colorwriter.Debug)
 	fmt.Fprintln(w, "a\tb\tc")
 	fmt.Fprintln(w, "aa\tbb\tcc")
 	fmt.Fprintln(w, "aaa\t") // trailing tab
@@ -58,7 +59,7 @@ func Example_trailingTab() {
 	// Observe that the third line has no trailing tab,
 	// so its final cell is not part of an aligned column.
 	const padding = 3
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, '-', tabwriter.AlignRight|tabwriter.Debug)
+	w := colorwriter.NewWriter(os.Stdout, 0, 0, padding, '-', colorwriter.AlignRight|colorwriter.Debug)
 	fmt.Fprintln(w, "a\tb\taligned\t")
 	fmt.Fprintln(w, "aa\tbb\taligned\t")
 	fmt.Fprintln(w, "aaa\tbbb\tunaligned") // no trailing tab
